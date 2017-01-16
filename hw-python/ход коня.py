f32e8743f134a1c82b4cca1c621736e310e7747e
veraivanova233 <veraivanova233@gmail.com>
2017-01-16 06:15:33 +0300

    Delete ход коня.py

--- hw-python/ход коня.py
+++
@@ -1,9 +0,0 @@
-a_y = int(input())
-a_x = int(input())
-b_y = int(input())
-b_x = int(input())
-if (b_x-a_x == 1 and (b_y - a_y == 2 or b_y - a_y == -2)) or (b_x - a_x == 2 and (b_y - a_y == 1 or b_y - a_y == -1)) or (b_x - a_x == -1 and (b_y-a_y == 2 or b_y - a_y == -2)) or (b_x - a_x ==-2 and (b_y - a_y == 1 or b_y - a_y == -1)):
-                                                                                                                                                                                     
-    print('YES')
-else:
-    print('NO')
