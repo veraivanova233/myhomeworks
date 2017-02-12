43d2837f3c9e3a43c5215f35203beb63a2a578db
veraivanova233 <veraivanova233@gmail.com>
2017-02-12 07:59:48 +0300

    Add files via upload

---
+++ hw-python/hw11.py
@@ -0,0 +1,16 @@
+import re
+f = open('text.txt', 'r', encoding = 'utf-8')
+y = open('text1.txt', 'w', encoding = 'utf-8')
+lines = f.readlines()
+for i in lines:
+    i = re.sub('(^Викинг)', 'Бурундук', i, flags = re.U)
+    i = re.sub('(викинг)', 'бурундук', i, flags = re.U)
+    i = re.sub('(Ви́кинги)', 'Бурундуки', i, flags = re.U)
+    i = str(i)
+    y.write(i)
+f.close()
+
+
+
+
+    
