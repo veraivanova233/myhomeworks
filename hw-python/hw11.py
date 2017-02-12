e80a3f3d7b15b4b654c0083b1ac7122164a65cc9
veraivanova233 <veraivanova233@gmail.com>
2017-02-12 08:00:47 +0300

    Update hw11.py

--- hw-python/hw11.py
+++ hw-python/hw11.py
@@ -5,7 +5,7 @@ lines = f.readlines()
 for i in lines:
     i = re.sub('(^Викинг)', 'Бурундук', i, flags = re.U)
     i = re.sub('(викинг)', 'бурундук', i, flags = re.U)
-    i = re.sub('(Ви́кинги)', 'Бурундуки', i, flags = re.U)
+    i = re.sub('(Ви́кинги)', 'Бурундуки́', i, flags = re.U)
     i = str(i)
     y.write(i)
 f.close()
